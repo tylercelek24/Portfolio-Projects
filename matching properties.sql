9f60f41ec3c136dcf8215d0ff82d4d6c4841584a
tylercelek24 <135525120+tylercelek24@users.noreply.github.com>
2024-04-12 10:49:11 -0400

    Add files via upload

--- matching properties.sql
+++ matching properties.sql
@@ -1,11 +1,20 @@
 /* matching properties */
-select lease.address, lease.SoldPrice, resi.MonthlyPayment, (lease.SoldPrice - resi.MonthlyPayment) as profit ,lease.Status, resi.Status
+with propmatch as (
+select sale.Address, sale.SoldPrice, sale.MonthlyPayment, lease.SoldPrice as rent, (lease.SoldPrice - sale.MonthlyPayment) as profit
 from (
-	select *
+	select Address, soldprice, MonthlyPayment, max(SettledDate) mostrecent
 	from Residential
-	where Category = 'resl') as lease
+	where Category = 'ress'
+	group by Address, soldprice, MonthlyPayment
+	) as sale
 join (
-	select *
+	select Address, soldprice, max(SettledDate) mostrecent
 	from Residential
-	where Category = 'ress') as resi
-on lease.address = resi.address;
+	where Category = 'resl'
+	group by Address, soldprice
+	) as lease
+on sale.address = lease.address)
+
+select *, avg(profit) over () as avg_profit
+from propmatch
+order by profit desc
