b3959e8b5ac88641332c9ff469350e7d228547cb
tylercelek24 <135525120+tylercelek24@users.noreply.github.com>
2024-04-12 10:48:15 -0400

    Add files via upload

--- alter records.sql
+++ alter records.sql
@@ -1,12 +1,7 @@
---Principal and interest
-update Residential
-set MonthlyPayment =
-	((SoldPrice - DownPayment20)*(Interest/12))/(1-(1/(power((1+(Interest/12)),360))));
-
-
---Add down payment (20%), interest (8%), and insurance
+--Add down payment (20%), interest (8%), insurance, and monthly payment
 Alter Table Residential
-ADD DownPayment20 int, Interest float, insurance float;
+ADD DownPayment20 int, Interest float, insurance float, MonthlyPayment int;
+
 
 Update Residential
 set DownPayment20 = SoldPrice*.2
@@ -21,16 +16,23 @@ set Insurance = round(SoldPrice*.0055556, 0)
 where category = 'ress';
 
 
+--Principal and interest
+update Residential
+set MonthlyPayment =
+	((SoldPrice - DownPayment20)*(Interest/12))/(1-(1/(power((1+(Interest/12)),360))));
+
+
 --Final monthly payment
 Update residential
 set MonthlyPayment = case
-	when associationfeefrequency = 'monthly' then monthlypayment + (TaxAnnualTotal/12) + (insurance/12) + AssociationFee
-	when associationfeefrequency = 'quarterly' then monthlypayment + (TaxAnnualTotal/12) + (insurance/12) + (AssociationFee/4)
-	when associationfeefrequency = 'annually' then monthlypayment + (TaxAnnualTotal/12) + (insurance/12) + (AssociationFee/12)
+	when associationfeefrequency = 'monthly' and associationfee is not null then monthlypayment + (TaxAnnualTotal/12) + (insurance/12) + AssociationFee
+	when associationfeefrequency = 'quarterly' and associationfee is not null then monthlypayment + (TaxAnnualTotal/12) + (insurance/12) + (AssociationFee/4)
+	when associationfeefrequency = 'semiannually' and associationfee is not null then monthlypayment + (TaxAnnualTotal/12) + (insurance/12) + (AssociationFee/6)
+	when associationfeefrequency = 'annually' and associationfee is not null then monthlypayment + (TaxAnnualTotal/12) + (insurance/12) + (AssociationFee/12)
 	else monthlypayment + (TaxAnnualTotal/12) + (insurance/12) end
 where Category = 'ress';
 
 
---Remove incomplete data
-delete from Residential
-where TaxAnnualTotal = 0 or TaxAnnualTotal > 100000;
+--Tax amounts for residential sale is required for calculations. Null values will be deleted from table.
+Delete from Residential
+where TaxAnnualTotal is null and category = 'ress';
